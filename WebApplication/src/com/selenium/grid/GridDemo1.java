b65524aa95654c422670229597416e291f8b5585
training <training@lenovo-PC>
2019-09-24 12:10:18 +0530

    yes

--- WebApplication/src/com/selenium/grid/GridDemo1.java
+++ WebApplication/src/com/selenium/grid/GridDemo1.java
@@ -1,6 +1,6 @@
 package com.selenium.grid;
 
-import org.openqa.selenium.By;
+\\\import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -9,10 +9,10 @@ import org.testng.annotations.Test;
 public class GridDemo1 {
 WebDriver driver;
 	
-	@BeforeTest
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\	@BeforeTest
 	public void test1() {
 		System.setProperty("webdriver.chrome.driver" ,
-				"C:\\Users\\Administrator\\Desktop\\Selenium_Jars\\chromedriver.exe");
+		\\\\\\\\\\\\\\\\\\		"C:\\Users\\Administrator\\Desktop\\Selenium_Jars\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.get("https://accounts.google.com/signin/v2/identifier?service=CPanel&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 	}
